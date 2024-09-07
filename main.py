9010537af42794605fefe3134fd1125ce7476e2d
yangio2005 <139154647+yangio2005@users.noreply.github.com>
2024-09-07 08:24:34 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import requests
+def get_news():
+    list_news[]
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Xin chào {update.effective_user.first_name}. Chúc {update.effective_user.first_name} một ngày tốt lành! 😊 ')
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'
+
+app = ApplicationBuilder().token("7384027524:AAHgL6Dz5rFOATYFrJLDkzt8vfvrfNj1AfM").build()
+
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("news", news))
+app.run_polling()
