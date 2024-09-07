8ce6184eed710d6a98d4dae2d5bcc7a7b645f213
yangio2005 <139154647+yangio2005@users.noreply.github.com>
2024-09-07 14:21:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -11,4 +15,21 @@ app = ApplicationBuilder().token("7384027524:AAHgL6Dz5rFOATYFrJLDkzt8vfvrfNj1AfM
 
 app.add_handler(CommandHandler("hello", hello))
 
+
+# Đường dẫn tới file JSON chứa thông tin xác thực
+SERVICE_ACCOUNT_FILE = 'path_to_your_service_account.json'
+
+# Phạm vi mà Google Sheets API yêu cầu
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Tải thông tin xác thực từ file JSON
+creds = None
+creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# Kết nối với Google Sheets
+client = gspread.authorize(creds)
+
+# Mở bảng tính Google Sheets với ID của nó
+SPREADSHEET_ID = 'your_spreadsheet_id_here'  # Thay thế bằng ID bảng tính của bạn
+sheet = client.open_by_key(SPREADSHEET_ID).sheet1
 app.run_polling()
